b903e92e8f6e1cede778ccedec556871d3f9e4fe
Sarah H. McGrath <sarah@shmcgrath.com>
2019-07-04 16:26:39 -0400

    Commit to catch dotfiles up

--- scripts/.scripts/espn-plus-dl.sh
+++ scripts/.scripts/espn-plus-dl.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-dateTimeRaw=$(date +%Y%m%d%H%M)
-echo "URL of the video to download:"
+dateTimeRaw=$(date +%Y%m%d%H%M%S)
+echo "URL of the video to download (search for m3u8 in the bar; wil be a 302):"
 read downloadURL
 youtube-dl -F "$downloadURL"
 echo "Format code of video to download:"
 read formatCode
-echo "Authorization header (if using Chrome from browser~ssai) (include \"authorization:)\":"
+echo "Authorization header (browser~ssai above the 302 request URL) (include \"authorization:)\":"
 read authorizationHeader
 echo "Save file name (do not include .mp4 or any extension):"
 read saveName
-fileName="$saveName-$dateTimeRaw"
-youtube-dl -f $formatCode "$downloadURL" --add-header="$authorizationHeader" -o ~/Videos/youtube-dl/$fileName.mp4
+fileName=$saveName-$dateTimeRaw
+youtube-dl -f $formatCode "$downloadURL" --add-header="$authorizationHeader" -o "/home/shmcg/Videos/youtube-dl/$fileName.mp4"

1e75107abf3716ecd70ba92f7f203cd9be84d5be
Sarah H. McGrath <sarah@shmcgrath.com>
2019-07-04 17:26:37 -0400

    Add dotfile split to tmux window 1

--- scripts/.scripts/tmux-shm.sh
+++ scripts/.scripts/tmux-shm.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 tmux has-session -t shm 2>/dev/null
 if [ $? != 0 ]; then
-    tmux new-session -s shm -n shell-notes -d
+    tmux new-session -s shm -n shell-note-dot -d
     tmux send-keys -t shm "cd $HOME" C-m
-    tmux split-window -v -p 20 -t shm
+    tmux split-window -v -p 30 -t shm
     tmux send-keys -t shm:1.2 "cd $HOME/Dropbox/notes" C-m
+    tmux split-window -h -p 50 -t shm:1.2
+    tmux send-keys -t shm:1.3 "cd $HOME/Dropbox/dotfiles" C-m
     if [ -d "$HOME/projects" ]; then
         tmux new-window -n dev -t shm
         tmux send-keys -t shm:2 "cd $HOME/projects" C-m
@@ -14,7 +16,7 @@ if [ $? != 0 ]; then
         if [ -d "$HOME/Documents/projects" ]; then
             tmux new-window -n dev -t shm
             tmux send-keys -t shm:2 "cd $HOME/Documents/projects/" C-m
-            tmux split-window -v -p 50 -t shm:2
+            tmux split-window -h -p 50 -t shm:2
             tmux send-keys -t shm:2.2 "cd $HOME/Documents/projects/" C-m
         fi
     fi 

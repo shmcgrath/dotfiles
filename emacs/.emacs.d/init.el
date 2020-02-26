6366c74fbc5f3a7b8f3e7e9a62a3412e20b8a3b3
Sarah H. McGrath <sarah@shmcgrath.com>
2020-02-25 23:18:01 -0500

    Add basics to init.el

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -14,15 +14,18 @@
 ;; Enable Evil
 (require 'evil)
 (evil-mode 1)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (evil))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+;; Disable GUI and visual bell
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(setq visible-bell 1)
+
+;; Text prompts
+(setq use-dialog-box nil)
+;; y or n instead of yes or no in text prompts
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; Store custom variables in an external file so init.el can be kept in source control
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(load custom-file 'noerror)

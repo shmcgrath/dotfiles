96101d693e6b6eebd6c8575d9924084ce8cff9df
Sarah H. McGrath <sarah@shmcgrath.com>
2020-02-28 22:41:32 -0500

    Update ESPN download script

--- scripts/.scripts/espn-plus-dl.sh
+++ scripts/.scripts/espn-plus-dl.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
 dateTimeRaw=$(date +%Y%m%d%H%M%S)
-echo "URL of the video to download (search for m3u8 in the bar; wil be a 302):"
+echo "search for m3u8 in the bar; will be a 302"
+echo "May use Firefox extension The Stream Detector; use master_desktop_complete stream for ESPN+"
+echo "URL of the video to download: "
 read downloadURL
 youtube-dl -F "$downloadURL"
 echo "Format code of video to download:"
 read formatCode
-echo "Authorization header (browser~ssai above the 302 request URL) (include \"authorization:)\":"
+echo "Auth Header is browser~ssai above the 302 request URL OR"
+echo "200 GET XHR octect stream; usually under 204 options"
+echo "Authorization header (include \"authorization:\"): "
 read authorizationHeader
-echo "Save file name (do not include .mp4 or any extension):"
+echo "Save file name (do not include .mp4 or any extension): "
 read saveName
 fileName=$saveName-$dateTimeRaw
 youtube-dl -f $formatCode "$downloadURL" --add-header="$authorizationHeader" -o "/home/shmcg/Videos/youtube-dl/$fileName.mp4"

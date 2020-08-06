c306bfc79fc7f38aa3bada3b9eabdf4e367e2550
Sarah H. McGrath <sarah@shmcgrath.com>
2020-08-06 17:27:52 -0400

    Simplify tmux script
    TODO: find out why .scripts is ignored in .gitignore

--- scripts/.scripts/tmux-shm.sh
+++ scripts/.scripts/tmux-shm.sh
@@ -1,36 +1,19 @@
 #!/usr/bin/env bash
 tmux has-session -t shm 2>/dev/null
 if [ $? != 0 ]; then
-    tmux new-session -s shm -n shell-note-dot -d
+    tmux new-session -s shm -n shell-note -d
     tmux send-keys -t shm "cd $HOME" C-m
     tmux split-window -v -p 30 -t shm
     tmux send-keys -t shm:1.2 "cd $HOME/Dropbox/notes" C-m
-    tmux split-window -h -p 50 -t shm:1.2
-    tmux send-keys -t shm:1.3 "cd $HOME/Dropbox/dotfiles" C-m
     if [ -d "$HOME/projects" ]; then
         tmux new-window -n dev -t shm
         tmux send-keys -t shm:2 "cd $HOME/projects" C-m
-        tmux split-window -v -p 30 -t shm:2
-        tmux send-keys -t shm:2.2 "cd $HOME/projects" C-m
-        tmux new-window -n shm-com -t shm
-        tmux send-keys -t shm:3 "cd $HOME/projects/shmcgrath-com && source ./venv/bin/activate" C-m
-        tmux split-window -v -p 20 -t shm:3
-        tmux send-keys -t shm:3.2 "cd $HOME/projects/shmcgrath-com && source ./venv/bin/activate" C-m
-        tmux send-keys -t shm:3.2 "invoke reserve" C-m
-
     else
         if [ -d "$HOME/Documents/projects" ]; then
             tmux new-window -n dev -t shm
             tmux send-keys -t shm:2 "cd $HOME/Documents/projects/" C-m
-            tmux split-window -h -p 30 -t shm:2
-            tmux send-keys -t shm:2.2 "cd $HOME/Documents/projects/" C-m
-            tmux new-window -n shm-com -t shm
-            tmux send-keys -t shm:3 "cd $HOME/projects/shmcgrath-com && source ./venv/bin/activate" C-m
-            tmux split-window -v -p 20 -t shm:3
-            tmux send-keys -t shm:3.2 "cd $HOME/projects/shmcgrath-com && source ./venv/bin/activate" C-m
-            tmux send-keys -t shm:3.2 "invoke reserve" C-m
         fi
-    fi 
+    fi
     tmux select-window -t shm:1
     tmux select-pane -U
 fi

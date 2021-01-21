25d15962c50840b243a620fe663e921319d2d2d4
Sarah H. McGrath <sarah@shmcgrath.com>
2021-01-20 20:30:12 -0500

    Update kali-shm.sh script

--- scripts/.scripts/kali-shm.sh
+++ scripts/.scripts/kali-shm.sh
@@ -1,8 +1,15 @@
+cd $HOME
 sudo mkdir /mnt/hgfs
 sudo mount -t fuse.vmhgfs-fuse .host:/ /mnt/hgfs -o allow_other
+echo 'ls /mnt/hgfs:'
 ls /mnt/hgfs
-
-#cd $HOME/dotfiles
-#stow git -t $HOME
-#stow vim -t $HOME
-#stow 
+ln --symbolic /mnt/hgfs/hacking-security $HOME/hacking-security
+ln --symbolic /mnt/hgfs/vm-share $HOME/vm-share
+mkdir $HOME/Dropbox
+ln --symbolic $HOME/hacking-security/bash-logs $HOME/Dropbox/bash-history 
+cd $HOME/dotfiles
+stow git -t $HOME
+stow vim -t $HOME
+stow tmux -t $HOME
+stow reference -t $HOME
+stow bookmarks -t $HOME

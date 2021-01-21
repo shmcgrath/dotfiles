45683f5db87b27c8de8e659ef3cee4875a9b8225
Sarah H. McGrath <sarah@shmcgrath.com>
2021-01-20 20:46:39 -0500

    correct window names in red tmux script

--- scripts/.scripts/tmux-red.sh
+++ scripts/.scripts/tmux-red.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 tmux has-session -t red 2>/dev/null
 if [ $? != 0 ]; then
-    tmux new-session -s red -n shell-note -d
+    tmux new-session -s red -n vpn -d
     tmux send-keys -t shm "cd $HOME" C-m
-	tmux new-window -n vpn -t red
+	tmux new-window -n enumeration -t red
     #tmux send-keys -t shm:1.2 "cd $HOME/Dropbox/notes" C-m
     tmux select-window -t red:1
     #tmux select-pane -U

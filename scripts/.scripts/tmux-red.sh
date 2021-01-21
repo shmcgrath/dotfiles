a78a5f49f78c381bdfffbcee7b5ff602283bac89
Sarah H. McGrath <sarah@shmcgrath.com>
2021-01-20 20:50:18 -0500

    Fix tmux-red

--- scripts/.scripts/tmux-red.sh
+++ scripts/.scripts/tmux-red.sh
@@ -2,9 +2,9 @@
 tmux has-session -t red 2>/dev/null
 if [ $? != 0 ]; then
     tmux new-session -s red -n vpn -d
-    tmux send-keys -t shm "cd $HOME" C-m
+    tmux send-keys -t red "cd $HOME/hacking-security/vpn" C-m
+    tmux send-keys -t red "echo 'to connect to vpn sudo openvpn'" C-m
 	tmux new-window -n enumeration -t red
-    #tmux send-keys -t shm:1.2 "cd $HOME/Dropbox/notes" C-m
     tmux select-window -t red:1
     #tmux select-pane -U
 fi

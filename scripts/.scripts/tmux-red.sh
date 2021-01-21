314824af3df829de563503a980571260272c7d78
Sarah H. McGrath <sarah@shmcgrath.com>
2021-01-20 21:16:51 -0500

    Add return chars and select correct window

--- scripts/.scripts/tmux-red.sh
+++ scripts/.scripts/tmux-red.sh
@@ -6,9 +6,9 @@ if [ $? != 0 ]; then
     tmux send-keys -t red "echo 'to connect to vpn sudo openvpn'" C-m
 	tmux new-window -n scratchpad -t red
 	tmux split-window -v -p 50 -t red:2
-	tmux send-keys -t red:2.2 "cd $HOME/hacking-security"
+	tmux send-keys -t red:2.2 "cd $HOME/hacking-security" C-m
 	tmux new-window -n enumeration -t red
-    tmux select-window -t red:1
+    tmux select-window -t red:22
     tmux select-pane -t red:2.2
 fi
 tmux attach -t red

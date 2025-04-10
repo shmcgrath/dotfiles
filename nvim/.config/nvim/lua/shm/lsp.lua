5bd8f17eb76571301289f83e4b54160c98e5ff36
Sarah H. McGrath <sarah@shmcgrath.com>
2025-04-09 20:59:52 -0400

    LSP setup changes

--- nvim/.config/nvim/lua/shm/lsp.lua
+++ nvim/.config/nvim/lua/shm/lsp.lua
@@ -14,27 +14,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
       -- client.server_capabilities.completionProvider.triggerCharacters = chars
       vim.lsp.completion.enable(true, client.id, args.buf, {autotrigger = true})
     end
-	--desc = "LSP actions",
-	--callback = function(event)
-		--local opts = { buffer = event.buf }
-
-		--vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-		--vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
-		--vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
-		--vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
-		--vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
-		--vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
-		--vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
-		--vim.keymap.set("n", "gc", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-		--vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
-		--vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
-    --grn in Normal mode maps to vim.lsp.buf.rename()
-    --grr in Normal mode maps to vim.lsp.buf.references()
-    --gri in Normal mode maps to vim.lsp.buf.implementation()
-    --gO in Normal mode maps to vim.lsp.buf.document_symbol() (this is analogous to the gO mappings in help buffers and :Man page buffers to show a “table of contents”)
-    --gra in Normal and Visual mode maps to vim.lsp.buf.code_action()
-    --CTRL-S in Insert and Select mode maps to vim.lsp.buf.signature_help()
-    --[d and ]d move between diagnostics in the current buffer ([D jumps to the first diagnostic, ]D jumps to the last)
 	end,
 })
 

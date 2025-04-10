e66f55818c841f511568c251b4c50b6ac415a49f
Sarah H. McGrath <sarah@shmcgrath.com>
2025-04-09 21:34:09 -0400

    Trying to fix LSP

--- nvim/.config/nvim/lua/shm/lsp.lua
+++ nvim/.config/nvim/lua/shm/lsp.lua
@@ -1,9 +1,6 @@
--- This is where you enable features that only work
--- if there is a language server active in the file
 vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup('my.lsp', {}),
-  callback = function(args)
-    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+  callback = function(ev)
+    local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
     if client:supports_method('textDocument/implementation') then
       -- Create a keymap for vim.lsp.buf.implementation ...
     end
@@ -12,7 +9,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
       -- Optional: trigger autocompletion on EVERY keypress. May be slow!
       -- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
       -- client.server_capabilities.completionProvider.triggerCharacters = chars
-      vim.lsp.completion.enable(true, client.id, args.buf, {autotrigger = true})
+      vim.lsp.completion.enable(true, client.id, ev.buf, {autotrigger = true})
     end
 	--desc = "LSP actions",
 	--callback = function(event)

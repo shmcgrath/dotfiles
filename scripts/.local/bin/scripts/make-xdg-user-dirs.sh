890e8788c3dc888692a73186564f17f7cd42471c
Sarah H. McGrath <sarah@shmcgrath.com>
2025-04-15 23:48:41 -0400

    Remove dropbox.py and xdg-dirs script

--- scripts/.local/bin/scripts/make-xdg-user-dirs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-[[ ! -d "$HOME/desktop" ]] && mkdir -p "$HOME/desktop"
-[[ ! -d "$HOME/downloads" ]] && mkdir -p "$HOME/downloads"
-[[ ! -d "$HOME/templates" ]] && mkdir -p "$HOME/templates"
-[[ ! -d "$HOME/public" ]] && mkdir -p "$HOME/public"
-[[ ! -d "$HOME/documents" ]] && mkdir -p "$HOME/documents"
-[[ ! -d "$HOME/music" ]] && mkdir -p "$HOME/music"
-[[ ! -d "$HOME/pictures" ]] && mkdir -p "$HOME/pictures"
-[[ ! -d "$HOME/videos" ]] && mkdir -p "$HOME/videos"

43009b6097b7b6990e867564d21e0dab8a6e53cb
Sarah H. McGrath <sarah@shmcgrath.com>
2025-05-05 19:47:52 -0400

    Fix shell configs

--- sh-base/.config/sh-base/functions/path_check_set-add_paths.sh
+++ sh-base/.config/sh-base/functions/path_check_set-add_paths.sh
@@ -1,41 +1,72 @@
-path_check_set () {
+validate_path() {
 	# checks the number of input arguments supplied
 	# if the arguments supplied equal 0, exit
 	if [ $# -eq 0 ]; then
-		printf "%s\n" "No arguments supplied to path_check_set. Failure 01"
+		printf "%s\n" "[E01] No arguments supplied to path_check_set."
 		return 1
 	fi
 
 	# checks if the first argument is an empty string
 	# if it is a null string, the printf is called
 	if [ -z "$1" ]; then
-		printf "%s\n" "Null string supplied to path_check_set. Failure 02"
+		printf "%s\n" "[E02] Null string supplied to path_check_set."
 		return 1
 	fi
 
-	# checks if directory exists
-	if [ -d "$1" ]; then
-		printf "%s\n" "Path to add for path_check_set: $1"
-		case ":$PATH:" in
-			*:"$1":*)
-				printf "%s\n" "Path already in PATH. Failure 03"
-				return 0
-				;;
-			*)
-				printf "%s\n" "Adding path to PATH."
-				PATH="$1":$PATH
-				export PATH
-				return 0
-				;;
-		esac
+	if [ ! -d "$1" ]; then
+		printf "%s\n" "[E03] Directory does not exist."
+		return 1
 	fi
-	printf "%s\n" "path_check_set did not add to PATH because $1 did not meet any criteria. Failure 04"
-	return 1
+}
+
+# Prepend a directory to PATH if it's not already in it
+prepend_path() {
+	validate_path "$1" || return 1
+
+	case ":$PATH:" in
+		"$1"[:]*)
+			printf "%s\n" "[E04] Path already first in PATH. Skipping: $1"
+			;;
+		*)
+			printf "%s\n" "Prepending to PATH: $1"
+			PATH="$1:$PATH"
+			export PATH
+			;;
+	esac
+}
+
+# Append a directory to PATH if it's not already in it
+append_path() {
+	validate_path "$1" || return 1
+
+	case ":$PATH:" in
+		*:"$1":*)
+			printf "%s\n" "[E05] Path already in PATH. Skipping: $1"
+			;;
+		*)
+			printf "%s\n" "Appending to PATH: $1"
+			PATH="$PATH:$1"
+			export PATH
+			;;
+	esac
 }
 
 add_paths() {
+	local method="append"
+
+	if [ "$1" = "--prepend" ]; then
+		method="prepend"
+		shift
+	fi
+
 	for dir in "$@"; do
-		path_check_set "$dir"
+		case "$method" in
+			prepend)
+				prepend_path "$dir" ;;
+			append)
+				append_path "$dir" ;;
+		esac
 	done
+
 	printf "%s\n" "\$PATH: $PATH"
 }

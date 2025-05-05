208a28fe63d4a347b2a2de4f33cf19f5ad2f2952
Sarah H. McGrath <sarah@shmcgrath.com>
2025-05-04 22:25:12 -0400

    Initial config for neovide

---
+++ nvim/.config/nvim/lua/shm/neovide.lua
@@ -0,0 +1,7 @@
+-- config for neovide
+-- additional options: https://neovide.dev/configuration.html
+vim.opt.guifont = "CommitMono Nerd Font Mono:h13"
+
+-- disable cursor animations
+vim.g.neovide_cursor_animation_length = 0
+vim.g.neovide_cursor_short_animation_length = 0

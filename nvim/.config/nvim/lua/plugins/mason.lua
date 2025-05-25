442520deac57d4e1ecf9eac00e4e314521d551af
Sarah H. McGrath <sarah@shmcgrath.com>
2025-05-24 21:54:40 -0400

    Add a function to have mason install tooling

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,4 +1,7 @@
-require("mason").setup({
+local mason = require("mason")
+local mason_registry = require("mason-registry")
+
+mason.setup({
   PATH = "append",
   ui = {
     border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
@@ -9,3 +12,30 @@ require("mason").setup({
     },
   },
 })
+
+-- tooling I want installed
+local tooling = {
+  -- LSPs
+  "lua-language-server",
+  -- DAPs
+  -- Linters & Formatters
+  "stylua",
+}
+
+-- install tooling listed above
+
+local function ensure_tooling_installed()
+  for _, name in ipairs(tooling) do
+    local ok, pkg = pcall(mason_registry.get_package, name)
+    if ok and not pkg:is_installed() then
+      pkg:install()
+    end
+  end
+end
+
+-- Check if registry needs to be refreshed (ie on first run or new plugin)
+if mason_registry.refresh then
+  mason_registry.refresh(ensure_tooling_installed)
+else
+  ensure_tooling_installed()
+end

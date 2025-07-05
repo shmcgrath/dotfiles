0d26bd10d48bec474dd91340117a8ff234287ec3
Sarah H. McGrath <sarah@shmcgrath.com>
2025-07-04 22:02:46 -0400

    Add more colors to cyberdream, increase readability

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -35,20 +35,20 @@ config.colors = {
   -- Cyberdream by https://github.com/scottmckendry/cyberdream.nvim
   -- I could not get my tab setup working with importing cyberdream, just copied it manually
   -- Cyberdream Dark Palette:
-  -- #16181a bg
-  -- #1e2124 bg_alt
-  -- #3c4048 bg_highlight
-  -- #ffffff fg
-  -- #7b8496 grey
-  -- #5ea1ff blue
-  -- #5eff6c green
-  -- #5ef1ff cyan
-  -- #ff6e5e red
-  -- #f1ff5e yellow
-  -- #ff5ef1 magenta
-  -- #ff5ea0 pink
-  -- #ffbd5e orange
-  -- #bd5eff purple
+  -- #16181a bg 0
+  -- #1e2124 bg_alt 235
+  -- #3c4048 bg_highlight 237
+  -- #ffffff fg 7
+  -- #7b8496 grey 247
+  -- #5ea1ff blue 4
+  -- #5eff6c green 2
+  -- #5ef1ff cyan 6
+  -- #ff6e5e red 1, 17
+  -- #f1ff5e yellow 3
+  -- #ff5ef1 magenta 207
+  -- #ff5ea0 pink 205
+  -- #ffbd5e orange 16
+  -- #bd5eff purple 5
 
   foreground = "#ffffff",
   background = "#16181a",
@@ -63,9 +63,35 @@ config.colors = {
   scrollbar_thumb = "#16181a",
   split = "#16181a",
 
-  ansi = { "#16181a", "#ff6e5e", "#5eff6c", "#f1ff5e", "#5ea1ff", "#bd5eff", "#5ef1ff", "#ffffff" },
-  brights = { "#3c4048", "#ff6e5e", "#5eff6c", "#f1ff5e", "#5ea1ff", "#bd5eff", "#5ef1ff", "#ffffff" },
-  indexed = { [16] = "#ffbd5e", [17] = "#ff6e5e" },
+  ansi = {
+    "#16181a",
+    "#ff6e5e",
+    "#5eff6c",
+    "#f1ff5e",
+    "#5ea1ff",
+    "#bd5eff",
+    "#5ef1ff",
+    "#ffffff"
+  },
+  brights = {
+    "#3c4048",
+    "#ff6e5e",
+    "#5eff6c",
+    "#f1ff5e",
+    "#5ea1ff",
+    "#bd5eff",
+    "#5ef1ff",
+    "#ffffff"
+  },
+  indexed = {
+    [16] = "#ffbd5e",
+    [17] = "#ff6e5e",
+    [205] = "#ff5ea0",
+    [207] = "#ff5ef1",
+    [235] = "#1e2124",
+    [237] = "#3c4048",
+    [247] = "#7b8496"
+  },
   tab_bar = {
     background = '#16181a',
     active_tab = {

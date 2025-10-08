e78a594950162017700ac9120a4667bd3f74addf
Sarah H. McGrath <sarah@shmcgrath.com>
2025-10-07 20:11:29 -0400

    River and Screenshot updates

--- scripts/.local/bin/menu-scripts/powermenu.sh
+++ scripts/.local/bin/menu-scripts/powermenu.sh
@@ -9,7 +9,7 @@ Shutdown
 river exit
 waybar restart"
 
-SELECTION=$(printf "%s\n" "$PWR_OPTIONS" | fuzzel --dmenu --lines 12 --prompt "Power Menu > ")
+SELECTION=$(printf "%s\n" "$PWR_OPTIONS" | fuzzel --dmenu --lines 12 --prompt "Power Menu > " --select="Suspend")
 
 case $SELECTION in
 	*"Lock")
--- scripts/.local/bin/menu-scripts/screenshot.sh
+++ scripts/.local/bin/menu-scripts/screenshot.sh
@@ -2,15 +2,27 @@
 
 OPTIONS="Selection to File
 Selection to Clipboard
-All Windows"
+Selection to Satty
+Monitor to File
+Monitor to Clipboard
+All Windows to File
+All Windows to Clipboard"
 
-SELECTION=$(printf "%s\n" "$OPTIONS" | fuzzel --dmenu --lines 12 --prompt "Screenshot > ")
+SELECTION=$(printf "%s\n" "$OPTIONS" | fuzzel --dmenu --lines 12 --prompt "Screenshot > " --select="Selection to Satty")
 
 case $SELECTION in
 	*"Selection to File")
 		grim -g "$(slurp)";;
 	*"Selection to Clipboard")
 		grim -g "$(slurp)" - | wl-copy;;
-	*"All Windows")
+	*"Selection to Satty")
+		grim -g "$(slurp)" - | satty --filename - --output-filename ~/Pictures/satty/$(date '+%Y%m%d-%H%M%S').png;;
+	*"Monitor to File")
+		grim -g "$(slurp -o)";;
+	*"Monitor to Clipboard")
+		grim -g "$(slurp -o)" - | wl-copy;;
+	*"All Windows to File")
 		grim;;
+	*"All Windows to Clipboard")
+		grim - | wl-copy;;
 esac

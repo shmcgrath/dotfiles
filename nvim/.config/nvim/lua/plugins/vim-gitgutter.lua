cafe3b3f26fd3c89cd2cd06a81d7dbc7662f98a2
Sarah H. McGrath <sarah@shmcgrath.com>
2025-12-26 19:10:29 -0500

    move vim-gitgutter config to lua

---
+++ nvim/.config/nvim/lua/plugins/vim-gitgutter.lua
@@ -0,0 +1,6 @@
+-- Settings for [vim-gitgutter](https://github.com/airblade/vim-gitgutter)
+vim.g.gitgutter_sign_added = "++"
+vim.g.gitgutter_sign_modified = "~~"
+vim.g.gitgutter_sign_removed = "--"
+vim.g.gitgutter_sign_removed_first_line = "^-"
+vim.g.gitgutter_sign_modified_removed = "~-"

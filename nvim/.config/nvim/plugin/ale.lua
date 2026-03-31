21b30cd3219ce2466d95d85a50c01e70fa8933e1
Sarah H. McGrath <sarah@shmcgrath.com>
2026-03-31 13:11:31 -0400

    Remove vim-sneak; format ale.lua

--- nvim/.config/nvim/plugin/ale.lua
+++ nvim/.config/nvim/plugin/ale.lua
@@ -6,7 +6,7 @@ vim.pack.add({
 
 -- Settings for [ale](https://github.com/dense-analysis/ale)
 vim.g.ale_fixers = {
-    ["*"] = { "remove_trailing_lines", "trim_whitespace" },
+  ["*"] = { "remove_trailing_lines", "trim_whitespace" },
 }
 
 vim.g.ale_sign_error = ">>"
@@ -15,7 +15,7 @@ vim.g.ale_lint_on_save = 0
 vim.g.ale_lint_on_text_changed = 0
 vim.g.ale_lint_on_enter = 0
 vim.g.ale_linters_explicit = 1
-vim.g.ale_virtualtext_cursor=0
+vim.g.ale_virtualtext_cursor = 0
 
 -- Error Message Formatting
 vim.g.ale_echo_msg_error_str = "E"
@@ -24,18 +24,18 @@ vim.g.ale_echo_msg_warning_str = "W"
 vim.g.ale_echo_msg_format = "[%severity%] [%linter%] %code: %%s"
 
 if vim.fn.has("nvim") == 1 then
-	vim.g.ale_use_neovim_diagnostics_api = 1
-	vim.g.ale_disable_lsp = 1
+  vim.g.ale_use_neovim_diagnostics_api = 1
+  vim.g.ale_disable_lsp = 1
 end
 
 vim.keymap.set("n", "<F6>", ":ALELint<CR>", {
-	noremap = true,
-	silent = false,
-	desc = "ALELint",
+  noremap = true,
+  silent = false,
+  desc = "ALELint",
 })
 
 vim.keymap.set("n", "<F7>", ":ALEFix<CR>", {
-	noremap = true,
-	silent = false,
-	desc = "ALEFix",
+  noremap = true,
+  silent = false,
+  desc = "ALEFix",
 })

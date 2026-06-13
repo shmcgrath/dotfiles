cd8f7890570d910ec6e216e60ac89b0c93dd91e9
Sarah H. McGrath <sarah@shmcgrath.com>
2026-06-13 18:13:51 -0400

    Add path fix for macos

--- nvim/.config/nvim/lua/shm/neovide.lua
+++ nvim/.config/nvim/lua/shm/neovide.lua
@@ -5,3 +5,6 @@ vim.opt.guifont = "CommitMono Nerd Font Mono:h13"
 -- disable cursor animations
 vim.g.neovide_cursor_animation_length = 0
 vim.g.neovide_cursor_short_animation_length = 0
+
+-- fix path on macos
+vim.env.PATH = vim.fn.system([[bash --login -c 'printf %s "$PATH"']]):gsub("\n$", "")

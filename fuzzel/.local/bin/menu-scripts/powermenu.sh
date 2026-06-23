b9dd0431c215170bc7bbb818e8815a1d21fa526c
Sarah H. McGrath <sarah@shmcgrath.com>
2026-06-22 22:25:23 -0400

    Update powermenu

--- fuzzel/.local/bin/menu-scripts/powermenu.sh
+++ fuzzel/.local/bin/menu-scripts/powermenu.sh
@@ -1,31 +1,38 @@
 #!/usr/bin/env bash
 
 PWR_OPTIONS="Lock
-Suspend
-Reboot
-Reboot to UEFI
 Hard reboot
+Reboot to UEFI
+Reboot
+Suspend
+swayidle
 Shutdown
 sway -> reload config
 sway -> restart
 sway -> exit
-river exit
 waybar restart
+dropbox systemd restart
+sway -> power monitors off
+sway -> power monitors on
+kill swayidle timer
 kanshi"
 
-SELECTION=$(printf "%s\n" "$PWR_OPTIONS" | fuzzel --dmenu --minimal-lines --prompt "Power Menu > " --select="Suspend")
+SELECTION=$(printf "%s\n" "$PWR_OPTIONS" | fuzzel --dmenu --minimal-lines --prompt "Power Menu > " --select="swayidle")
 
 case $SELECTION in
 	*"Lock")
 		swaylock;;
-	*"Suspend")
-		swaylock && systemctl suspend;;
-	*"Reboot")
-		systemctl reboot;;
+	*"Hard reboot")
+		#pkexec "echo b > /proc/sysrq-trigger";;
+		pkexec sh -c 'echo 1 > /proc/sys/kernel/sysrq; echo b > /proc/sysrq-trigger';;
 	*"Reboot to UEFI")
 		systemctl reboot --firmware-setup;;
-	*"Hard reboot")
-		pkexec "echo b > /proc/sysrq-trigger";;
+	*"Reboot")
+		systemctl reboot;;
+	*"Suspend")
+		swaylock && sleep 3 && systemctl suspend;;
+	*"swayidle")
+		sleep 3 && pkill -SIGUSR1 --exact swayidle;;
 	*"Shutdown")
 		systemctl poweroff;;
 	*"sway -> reload config")
@@ -34,10 +41,16 @@ case $SELECTION in
 		swaymsg restart;;
 	*"sway -> exit")
 		swaymsg exit;;
-	*"river exit")
-		riverctl exit;;
 	*"waybar restart")
 		systemctl --user restart waybar@*;;
+	*"dropbox systemd restart")
+		systemctl --user restart dropbox;;
+	*"sway -> power monitors off")
+		swaymsg "output * power off";;
+	*"sway -> power monitors on")
+		swaymsg "output * power on";;
+	*"kill swayidle timer")
+		pkill --exact swayidle && systemctl --user restart swayidle.service
 	*"kanshi")
-		kanshi &;;
+		systemctl --user restart kanshi;;
 esac

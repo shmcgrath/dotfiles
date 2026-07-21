662fcf9d49f8cfdd97a3a612bf39766b7ff24fc1
Sarah H. McGrath <sarah@shmcgrath.com>
2026-07-21 13:46:09 -0400

    Remove newsboat config

--- newsboat/.config/newsboat/shm-bookmark-cmd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-URL="$1"
-TITLE="$2"
-DESCRIPTION="$3"
-FEED_TITLE="$4"
-EXTENDED="Description: ${DESCRIPTION} Feed: ${FEED_TITLE}"
-
-echo -e "[${TITLE}](${URL})${DESCRIPTION}\t${FEED_TITLE}" >> ~/bookmarks.txt
